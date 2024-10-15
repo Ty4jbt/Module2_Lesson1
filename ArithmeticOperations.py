d590e3b9db3c37884bcc4303649f1f29d276c8df
Ty4jbt <tyaboe@gmail.com>
2024-10-14 22:00:14 -0700

    first commit

---
+++ ArithmeticOperations.py
@@ -0,0 +1,14 @@
+# Task 1
+cost_of_bread, cost_of_peanut_butter, cost_of_jelly = 5, 6, 4
+
+total_cost = cost_of_bread + cost_of_peanut_butter + cost_of_jelly
+print(total_cost)
+
+# Task 2
+initial_savings = 10000
+
+interest_rate = 0.07
+
+savings_after_1_year = initial_savings + (initial_savings * interest_rate)
+
+print(savings_after_1_year)
---
+++ ShowcaseYourDanceMoves.py
@@ -0,0 +1,17 @@
+# Task 1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Task 2
+how_are_you = input("How are you feeling today? ")
+
+if how_are_you == "happy":
+    print("That's great to hear!")
+elif how_are_you == "sad":
+    print("I hope your day gets better!")
+else:
+    print("Please input either 'happy' or 'sad'.")
